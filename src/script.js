01c80e022ae77d319110dcb6cc697a358449b4f7
David Omanovic <117093972+davidomanovic@users.noreply.github.com>
2024-11-18 17:34:09 +0100

    Create script.js

---
+++ src/script.js
@@ -0,0 +1,3 @@
+document.getElementById('download-btn').addEventListener('click', () => {
+    window.location.href = 'path-to-your-exe-file.exe'; // Replace with the actual path to your .exe file
+});
